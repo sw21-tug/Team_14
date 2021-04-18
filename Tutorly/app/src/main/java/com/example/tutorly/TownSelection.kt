81d9554b4f5cdfea2f26dfdf62bd8e02b23769ab
Mergim G <mgashi@student.tugraz.at>
2021-04-18 22:17:40 +0200

    TL-004: getting location with the help of the google api [SM, GM]

--- Tutorly/app/src/main/java/com/example/tutorly/TownSelection.kt
+++ Tutorly/app/src/main/java/com/example/tutorly/TownSelection.kt
@@ -1,18 +1,52 @@
 package com.example.tutorly
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 class TownSelection : AppCompatActivity() {
 
     private val mTopToolbar: Toolbar? = null
+    lateinit var mFusedLocationClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_town_selection)
         setSupportActionBar(findViewById(R.id.titleToolbar))
 
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
+
+        findViewById<Button>(R.id.btnGetlocation).setOnClickListener {
+            getLocation()
+        }
     }
+
+    private fun getLocation() {
+
+        val task = mFusedLocationClient.lastLocation
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 101
+            )
+            return
+        }
+
+        task.addOnSuccessListener {
+            if (it != null) {
+                Toast.makeText(applicationContext, "${it.longitude} ${it.latitude}", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+
 }
