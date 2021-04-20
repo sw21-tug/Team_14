9bdd3b8de39ff1874c9fc3a4ab1f38d897b612e9
Sarac <muhammed.sarac@student.tugraz.at>
2021-04-20 23:01:19 +0200

    TL-004: addrress autocompletion with google api [SM, GM]

--- Tutorly/app/src/main/java/com/example/tutorly/TownSelection.kt
+++ Tutorly/app/src/main/java/com/example/tutorly/TownSelection.kt
@@ -4,17 +4,32 @@ import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import com.google.android.gms.common.api.Status
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.api.net.PlacesClient
+import com.google.android.libraries.places.widget.Autocomplete
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode
+import javax.net.ssl.SSLEngineResult
 
 class TownSelection : AppCompatActivity() {
 
     private val mTopToolbar: Toolbar? = null
     lateinit var mFusedLocationClient: FusedLocationProviderClient
+
+    lateinit var placesClient:PlacesClient
+    private var placeFields = listOf(Place.Field.ID, Place.Field.NAME, Place.Field.ADDRESS)
+
+    //lateinit var placesClient:PlacesClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +42,8 @@ class TownSelection : AppCompatActivity() {
         findViewById<Button>(R.id.btnGetlocation).setOnClickListener {
             getLocation()
         }
+
+        placesAutocomplete()
     }
 
     private fun getLocation() {
@@ -47,6 +64,33 @@ class TownSelection : AppCompatActivity() {
             }
         }
     }
+
+    private fun placesAutocomplete(){
+        val autocompleteFragment = supportFragmentManager.findFragmentById(R.id.autocomplete_fragment) as AutocompleteSupportFragment
+
+        Places.initialize(this, getString(R.string.API))
+        placesClient = Places.createClient(this)
+
+        autocompleteFragment.setPlaceFields(placeFields)
+
+        autocompleteFragment.setHint("f.e Graz")
+
+        autocompleteFragment.setOnPlaceSelectedListener(object : PlaceSelectionListener {
+            override fun onPlaceSelected(p0: Place) {
+                //Log.i(TAG, "Place: ${place.name}, ${place.id}")
+                Toast.makeText(this@TownSelection, ""+p0.address, Toast.LENGTH_SHORT).show()
+                val text = findViewById<TextView>(R.id.textView)
+                text.setText(p0.address)
+            }
+
+            override fun onError(p0: Status) {
+                Toast.makeText(this@TownSelection, ""+p0.statusMessage, Toast.LENGTH_SHORT).show()
+            }
+
+        })
+    }
+
+
 
 
 }
