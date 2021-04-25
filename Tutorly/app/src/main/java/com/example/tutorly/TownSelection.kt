a4a6055535c1d5aef30487c6c76d575bd6e2d620
Mergim G <mgashi@student.tugraz.at>
2021-04-26 00:18:50 +0200

    Tl-004 design changes of the button and fragment [GM,SE]

--- Tutorly/app/src/main/java/com/example/tutorly/TownSelection.kt
+++ Tutorly/app/src/main/java/com/example/tutorly/TownSelection.kt
@@ -10,8 +10,6 @@ import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.common.api.Status
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
 import com.google.android.libraries.places.api.Places
 import com.google.android.libraries.places.api.model.Place
 import com.google.android.libraries.places.api.net.PlacesClient
@@ -19,12 +17,12 @@ import com.google.android.libraries.places.widget.Autocomplete
 import com.google.android.libraries.places.widget.AutocompleteSupportFragment
 import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
 import com.google.android.libraries.places.widget.model.AutocompleteActivityMode
+import java.util.*
 import javax.net.ssl.SSLEngineResult
 
 class TownSelection : AppCompatActivity() {
 
-    private val mTopToolbar: Toolbar? = null
-    lateinit var mFusedLocationClient: FusedLocationProviderClient
+
 
     lateinit var placesClient:PlacesClient
     private var placeFields = listOf(Place.Field.ID, Place.Field.NAME, Place.Field.ADDRESS)
@@ -36,34 +34,11 @@ class TownSelection : AppCompatActivity() {
         setContentView(R.layout.activity_town_selection)
         setSupportActionBar(findViewById(R.id.titleToolbar))
 
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
-
-        findViewById<Button>(R.id.btnGetlocation).setOnClickListener {
-            getLocation()
-        }
 
         placesAutocomplete()
     }
 
-    private fun getLocation() {
-
-        val task = mFusedLocationClient.lastLocation
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-            && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
-        ) {
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 101
-            )
-            return
-        }
-
-        task.addOnSuccessListener {
-            if (it != null) {
-                Toast.makeText(applicationContext, "${it.longitude} ${it.latitude}", Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
 
     private fun placesAutocomplete(){
         val autocompleteFragment = supportFragmentManager.findFragmentById(R.id.autocomplete_fragment) as AutocompleteSupportFragment
@@ -73,12 +48,13 @@ class TownSelection : AppCompatActivity() {
 
         autocompleteFragment.setPlaceFields(placeFields)
 
-        autocompleteFragment.setHint("f.e Graz")
+        autocompleteFragment.setHint("City..")
+
 
         autocompleteFragment.setOnPlaceSelectedListener(object : PlaceSelectionListener {
             override fun onPlaceSelected(p0: Place) {
                 //Log.i(TAG, "Place: ${place.name}, ${place.id}")
-                Toast.makeText(this@TownSelection, ""+p0.address, Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@TownSelection, "You have selected "+p0.address, Toast.LENGTH_SHORT).show()
                 val text = findViewById<TextView>(R.id.textView)
                 text.setText(p0.address)
             }
@@ -89,7 +65,6 @@ class TownSelection : AppCompatActivity() {
 
         })
     }
-
 
 
 
