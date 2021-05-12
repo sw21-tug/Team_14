271946477d1f19fba9c4fe87ec8d0e4e74efe402
imic <imic@student.tugraz.at>
2021-05-12 12:12:38 +0200

    TL-006 basic activity layout [MI, SM]

---
+++ Tutorly/app/src/main/java/com/example/tutorly/TutorProfile.kt
@@ -0,0 +1,11 @@
+package com.example.tutorly
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TutorProfile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tutor_profile)
+    }
+}
