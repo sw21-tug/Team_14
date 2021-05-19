97d86cc80373dac030b8ef4a8296dadcaa7d4261
Timo Edlinger <timo@pop-os.localdomain>
2021-05-19 10:16:17 +0200

    TL-005 added TutorsRecyclerView [ET,SM]

--- Tutorly/app/src/main/java/com/example/tutorly/TutorsRecyclerView.kt
+++ Tutorly/app/src/main/java/com/example/tutorly/TutorsRecyclerView.kt
@@ -1,4 +1,58 @@
 package com.example.tutorly
 
-class TutorsRecyclerView {
+import android.content.Context
+import android.graphics.Color
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.RelativeLayout
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.card.MaterialCardView
+
+class TutorsRecyclerView(private val context: Context, private val tutors: ArrayList<Tutor>) : RecyclerView.Adapter<TutorsRecyclerView.ViewHolder>() {
+
+    lateinit var holder: ViewHolder
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val cardView: MaterialCardView
+        val tutorNameView: TextView
+        val tutorImage: ImageView
+
+        init {
+            tutorNameView = view.findViewById(R.id.tutors_name)
+            tutorImage = view.findViewById(R.id.tutors_image)
+            cardView = view.findViewById(R.id.tutors_card_view)
+
+        }
+    }
+
+    fun getTutors() : ArrayList<Tutor>{
+
+        return tutors
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.tutors_list_layout, parent, false)
+        holder = ViewHolder(view)
+
+        return holder
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.tutorNameView.text = tutors[position].tutor_name
+        holder.tutorImage.setImageResource(R.drawable.ic_launcher_foreground)
+        holder.cardView.setOnClickListener {view: View ->
+                // TODO get to information activity of the tutor
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return tutors.size
+    }
+
 }
